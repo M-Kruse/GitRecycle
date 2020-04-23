59d22c19bcd381d230a9fad24daa30340d0948f1
M-Kruse <matthewdkruse@comcast.net>
2020-04-23 15:57:06 -0700

    adding auth tokens

--- GitRecycle/GitRecycle/urls.py
+++ GitRecycle/GitRecycle/urls.py
@@ -2,9 +2,10 @@ from django.conf.urls import url
 from django.urls import path, include
 from rest_framework import routers, serializers, viewsets
 from django.contrib import admin
-
+from rest_framework.authtoken.views import obtain_auth_token
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path('api/', include('API.urls')),
+	path('api-token-auth/', obtain_auth_token, name='api-tokn-auth'), 
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
